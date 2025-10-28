ec5ac08d260b932b42e60c21804f2017764297cd
kajaldhawley <kajaldhawley@gmail.com>
2025-10-28 18:20:21 +0000

    add setup.py n file setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    functions will return list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        [req.replace("\n","") for req in requirements]
+
+setup(
+name ='mymlproject',
+version ='0.0.1',
+author = 'Kajal',
+author_email='kajaldhawley@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
